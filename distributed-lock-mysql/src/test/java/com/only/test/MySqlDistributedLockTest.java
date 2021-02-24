b31c2a953ac30080be4d015d7659ce8efc8ec743
ouyangliuy <745252780@qq.com>
2021-02-24 23:06:51 +0800

    mysql 测试分布式锁

--- distributed-lock-mysql/src/test/java/com/only/test/MySqlDistributedLockTest.java
+++ distributed-lock-mysql/src/test/java/com/only/test/MySqlDistributedLockTest.java
@@ -7,6 +7,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import java.util.Arrays;
+import java.util.Random;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 import java.util.concurrent.TimeUnit;
 
 @RunWith(SpringRunner.class)
@@ -19,5 +23,28 @@ public class MySqlDistributedLockTest {
     public void testLock(){
 
         System.out.println(mySqlDistributedLock.getLock("testLock",100L, TimeUnit.SECONDS));
+    }
+
+    @Test
+    public void mysqlLockTest(){
+        ExecutorService executorService = Executors.newCachedThreadPool();
+        int[] count = {0};
+        for (int i = 0; i < 10; i++) {
+            int finalI = i;
+            executorService.submit(()->{
+                try {
+                    //mySqlDistributedLock.getLock("mysqlLockTest",100L,TimeUnit.SECONDS);
+                    //Thread.sleep(finalI);
+                    count[0] ++ ;
+                }catch (Exception e){
+                    e.printStackTrace();
+                }finally {
+                   // mySqlDistributedLock.releaseLock("mysqlLockTest");
+                    System.out.println("dd");
+                }
+            });
+
+        }
+        System.out.println(Arrays.toString(count));
     }
 }
